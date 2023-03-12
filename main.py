bc1006827bbcd329fbea7215dd94d2fb77dee828
Ashwani Kumar <ashwani.oct8@gmail.com>
2023-03-12 16:05:26 -0500

    almost complete

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-money_machine = MoneyMachine() # object created and stored
+money_machine = MoneyMachine()  # object created and stored
 coffee_maker = CoffeeMaker()
 menu = Menu()
 
@@ -16,4 +16,6 @@ while is_on:
         money_machine.report()
     else:
         drink = menu.find_drink(choice)
-        print(drink)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
